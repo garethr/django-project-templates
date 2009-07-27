1273d256a0312d46a816f7972e92b4740b9713f8
Gareth Rushgrove <gareth@morethanseven.net>
2009-07-27 21:46:00 +0100

    the package now includes the templates, they needed to be added to the MANIFEST

--- setup.py
+++ setup.py
@@ -2,19 +2,32 @@ from setuptools import setup, find_packages
 import os
 
 setup(
-    name='django_project_templates',
-    description="Paster templates for creating Django applications as eggs",
+    name='django-project-templates',
+    version = "0.4",
+    description="Paster templates for creating Django applications",
     author='Gareth Rushgrove',
     author_email='gareth@morethanseven.net',
+    url='http://github.com/garethr/django-project-templates/',
     packages = find_packages('src'),
     package_dir = {'':'src'},
+    license = "MIT",
+    keywords = "django paster",
     install_requires=[
         'setuptools',
         'PasteScript>=1.3',
         'Cheetah',
+        'fabric',
     ],
+    include_package_data=True,
+    zip_safe=False,
     entry_points="""
         [paste.paster_create_template]
         django_project=django_project_templates.pastertemplates:DjangoProjectTemplate
     """,
+    classifiers = [
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
 )
